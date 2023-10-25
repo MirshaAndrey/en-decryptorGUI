8b321f1da8ae4188293245e100866f40fde20e4e
MirshaAndrey <mirsha.andrey@gmail.com>
2023-10-25 23:02:00 +0300

    v0.1

---
+++ main.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+import pyAesCrypt
+from tkinter import filedialog
+from tkinter import messagebox
+
+# Функция для шифрования файла
+def encrypt_file():
+    password = password_entry.get()
+    if not password:
+        messagebox.showerror("Error", "Please enter a password.")
+        return
+
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        encrypted_file_path = file_path + ".aes"
+        pyAesCrypt.encryptFile(file_path, encrypted_file_path, password)
+        messagebox.showinfo("Success", "File encrypted successfully!")
+
+# Функция для дешифрования файла
+def decrypt_file():
+    password = password_entry.get()
+    if not password:
+        messagebox.showerror("Error", "Please enter a password.")
+        return
+
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        decrypted_file_path = file_path.replace(".aes", "_out")
+        pyAesCrypt.decryptFile(file_path, decrypted_file_path, password)
+        messagebox.showinfo("Success", "File decrypted successfully!")
+
+# Создание графического интерфейса
+root = tk.Tk()
+root.geometry("300x250")
+root.iconbitmap(default="favicon.ico")
+root.title("File En/Decryptor")
+
+password_label = tk.Label(root, text="Enter Password:")
+password_label.pack()
+
+password_entry = tk.Entry(root, show="*")  # Пароль будет скрыт
+password_entry.pack()
+
+encrypt_button = tk.Button(root, text="Encrypt File", command=encrypt_file)
+encrypt_button.pack()
+
+
+decrypt_button = tk.Button(root, text="Decrypt File", command=decrypt_file)
+decrypt_button.pack()
+
+root.mainloop()
